7e739b150d2b7b9de119f333f3a5586e5c6de42f
LH <lindsaynhenderson@gmail.com>
2022-09-09 14:58:33 -0500

    start writing functions'

---
+++ js/app.js
@@ -0,0 +1,109 @@
+console.log('bananas')
+
+// Start of game: 
+
+// Set Fox’s status:  
+
+// >Energy(food), 
+
+// >miles run,
+
+// >rest
+
+// >cookie
+
+
+// Set a daily mileage quota (ie 2 miles).  (Each day mileage quota will increase +1)
+
+// Set “Run” button = .25 miles, value of button will increase by .25 every day.
+
+// Set a daily rest quota - //this may or may not change daily, i haven’t decided//
+
+// ————————
+
+// Food and rest increase Fox’s energy
+
+// Establish 2 types of food: sandwich and cookie. Sandwich increases energy. Cookie increases energy less than sandwich, but Fox still must eat one every day.
+
+
+// >When fox eats sandwich: energy level increases +2
+
+// >When fox eats cookie: energy level increases + 1, AND cookie quota has been met
+
+
+// Max energy level is 5. Fox won’t eat if energy is at max, or if the energy value of a food would make Fox’s energy level exceed 5.
+
+
+// When Fox runs, energy level decreases by 2 for every 1 mile
+
+// If energy is below 2 when the “run” button is pressed, Fox won’t run.
+
+
+// Use timer to establish end of “day”
+
+// at end of day assess fox’s status:
+
+// >If the mileage quota is not met OR the rest quota is not met, OR they did not get a cookie, 
+
+// Fox cannot progress to the next day. 
+
+// Timer is reset with the day’s same distance and rest quotas.
+
+// >If mileage, rest, and cookie quotas are met 
+
+// Timer is reset
+
+// Fox’s mileage quota will increase
+
+// Value of “run” button will increase
+
+// >if quotas are met at the end of day 5, Fox is ready for a 5k and you win!
+
+
+// Will need these buttons:
+
+// Run
+
+// Food
+
+// Rest
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+// let runLog = 
+// let runGoal =
+// let restLog = 
+// let cookieTime = 
+
+/*------------------------ Cached Element References ------------------------*/
+
+const goRun = document.querySelector("#go-run")
+const goFeed = document.querySelector("#go-feed")
+const goRest = document.querySelector("#go-rest")
+const resetBtn = document.querySelector(".reset")
+const startBtn = document.querySelector("#start")
+/*----------------------------- Event Listeners -----------------------------*/
+
+resetBtn.addEventListener('click', resetClick)
+goRun.addEventListener('click', logRun)
+goFeed.addEventListener('click', increaseEnergy)
+startBtn.addEventListener('click', startWeek)
+
+/*-------------------------------- Functions --------------------------------*/
+
+function init(){
+let runLog = 0
+let runGoal =0
+let restLog = 0
+let cookieTime = 0
+}
+
+function startWeek {
+    let runlog =
+}
+
+
